d3785b3b7371cc80c0f2d6c351e839deae0b9a6b
Vi Figueiredo <vifigcode@gmail.com.br>
2024-06-10 16:08:57 -0300

    "Added DownloadFiles service to main.py and updated ExtractFiles.pyc"

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from services import CriarDiretorios
 from services import ColetarUrls
+from services import DownloadFiles
 from services import ExtractFiles
 
 async def main():
   CriarDiretorios.make()
   ColetarUrls.scrape()
+  DownloadFiles.download()
   ExtractFiles.extract()
   ExtractFiles.add_extension()
 
